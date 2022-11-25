3b7294717f62667fea6786a5bbe57d30baebd00a
Shamil Irfan <113251308+shamilirfan@users.noreply.github.com>
2022-11-25 14:26:02 -0500

    shamil irfan (#1)
    * show & hide learning panel
    
    * shamil irfan

--- src/Learning/Quiz.js
+++ src/Learning/Quiz.js
@@ -12,8 +12,7 @@ function Quiz({showHadith}) {
 
     return (
         <div className= "flex items-center justify-center flex-[25] w-[25vw] h-[100vh] sticky top-0">
-            <div className= "flex flex-col items-center justify-center border-[1px] border-[#41B6BD] rounded-xl w-[35vw] h-[98vh]
-                             m-[10px] pt-[5px] pb-[5px] pl-[10px] pr-[10px] relative overflow-hidden bg-[#FFFFFF]">
+            <div className= "flex flex-col items-center justify-center border-[1px] border-[#41B6BD] rounded-xl w-[35vw] h-[98vh] m-[10px] pt-[5px] pb-[5px] pl-[10px] pr-[10px] relative overflow-hidden bg-[#FFFFFF]">
                 <QuizHeader {...{state, next}}/>
                 <div className="w-[98%] pt-[15px] pb-[15px] pl-[8px] pr-[8px] absolute flex flex-col top-[85px] left-0 right-0 bottom-0 items-center justify-center overflow-y-auto overflow-x-hidden">
                     <div className="flex flex-auto justify-center flex-col max-h-[80vh]">
